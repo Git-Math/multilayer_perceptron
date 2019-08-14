4b7e53fb1736d85b3a61c5cc50392d06869cf95c
Git-Math <matoo3@hotmail.fr>
2019-08-14 06:19:19 +0200

    initialize and feedforward

--- mp_train.py
+++ mp_train.py
@@ -2,22 +2,37 @@ import sys
 import os
 import csv
 import numpy as np
+import random
 
 import utils
 from utils import error
-
-feature_number = 30
 
 def usage():
     error('%s [dataset]' % sys.argv[0])
 
 def scale(feature_matrix):
-    min_matrix = np.min(feature_matrix, axis = 0)
-    max_matrix = np.max(feature_matrix, axis = 0)
+    min_matrix = np.min(feature_matrix, axis = 1).reshape(-1, 1)
+    max_matrix = np.max(feature_matrix, axis = 1).reshape(-1, 1)
     scaled_feature_matrix = (feature_matrix - min_matrix) / (max_matrix - min_matrix)
     return scaled_feature_matrix
 
-def read_data(filename):
+def relu(z):
+    return np.maximum(0, z)
+
+def softmax(z):
+    return np.exp(z) / np.sum(np.exp(z), axis=0)
+
+def feedforward(wb, x, layers, neurons):
+    za = []
+    for i in range(layers - 1):
+        prev_x = x if i == 0 else za[i - 1]["a"]
+        z = np.dot(wb[i]["w"], prev_x) + wb[i]["b"]
+        a = softmax(z) if i == layers - 2 else relu(z)
+        za.append({ "z": z, "a": a})
+    y_mp = za[layers - 2]["a"]
+    return y_mp, za
+
+def read_data(filename, feature_number):
     # checks
     if not os.path.isfile(filename):
         error('no such file: %s' % filename)
@@ -28,15 +43,15 @@ def read_data(filename):
             reader = csv.reader(fs)
             row_number = sum(1 for row in reader)
             fs.seek(0)
-            x = np.empty([row_number, feature_number])
-            y = np.empty([row_number, 1])
+            x = np.empty([feature_number, row_number])
+            y = np.empty([1, row_number])
             i_row = 0
             for row in reader:
                 for i, field in enumerate(row):
                     if i == 1:
-                        y[i_row][0] = 1.0 if field == "M" else 0.0
+                        y[0][i_row] = 1.0 if field == "M" else 0.0
                     elif i >= 1:
-                        x[i_row][i - 2] = float(field)
+                        x[i - 2][i_row] = float(field)
                 i_row += 1
     except:
         error("invalid dataset")
@@ -49,7 +64,17 @@ if __name__ == '__main__':
     if not os.path.isfile(sys.argv[1]):
         error('no such file: %s' % sys.argv[1])
 
-    x, y = read_data(sys.argv[1])
+    feature_number = 30
+    x, y = read_data(sys.argv[1], feature_number)
     x = scale(x)
-    print(x)
-    
+    layers = 4
+    neurons = [feature_number, int(feature_number * 2 / 3 + 1), int(feature_number * 2 / 3 + 1), 2]
+    learning_rate = 0.01
+    # He initialization
+    np.random.seed(0)
+    wb = []
+    for i in range(layers - 1):
+        w = np.random.randn(neurons[i + 1], neurons[i]) / np.sqrt(neurons[i])
+        b = np.random.randn(neurons[i + 1], 1)
+        wb.append({ "w": w, "b": b})
+    y_mp, za = feedforward(wb, x, layers, neurons)
